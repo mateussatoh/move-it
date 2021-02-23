e847fab1c8ee324906d0b741848e1646aa077128
Mateus Satoh <mateussatoh@gmail.com>
2021-02-23 11:40:06 -0300

    add: countdown button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,16 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { Countdown } from "../components/Countdown";
 
+import Head from "next/head";
+
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
